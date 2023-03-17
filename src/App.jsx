bee8e963b35bdb6e4c0050e49d75298cfd0914a3
julia <julia.albq@gmail.com>
2023-03-17 15:38:22 -0300

    implement logic to render the correct components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import { getSelectedQuiz } from "./state/allQuizzes";
 import { getQuizCreation, quizCreatedAction } from "./state/quiz";
 import { Button } from "./ui/components/Button";
 import { CreateQuiz } from "./ui/components/CreateQuiz";
+import { QuizList } from "./ui/components/QuizList";
+import { SelectedQuiz } from "./ui/components/SelectedQuiz";
 
 function App() {
   const quizCreation = useSelector((state) => getQuizCreation(state));
+  const selectedQuiz = useSelector((state) => getSelectedQuiz(state));
   const dispatch = useDispatch();
 
   const createQuizPage = () => {
@@ -14,8 +18,18 @@ function App() {
 
   return (
     <div className="App">
-      <Button onClick={createQuizPage}>Create Quiz</Button>
-      {quizCreation && <CreateQuiz />}
+      <div className="create-quiz__container">
+        {Object.keys(selectedQuiz).length === 0 && (
+          <Button onClick={createQuizPage}>Create Quiz</Button>
+        )}
+
+        {quizCreation && <CreateQuiz />}
+
+        {Object.keys(selectedQuiz).length > 0 && (
+          <SelectedQuiz quiz={selectedQuiz} />
+        )}
+      </div>
+      <QuizList />
     </div>
   );
 }
