666767c7163979b3eb4721d99b56b41c1bb019bb
julia <julia.albq@gmail.com>
2023-03-17 15:34:41 -0300

    feat: add redux logic related to loading quizzes

---
+++ src/state/allQuizzes.js
@@ -0,0 +1,85 @@
+import axios from "axios";
+import { apiUrl } from "../infrastructure/apiUrl";
+
+const allQuizzesInitialState = {
+  loading: false,
+  failed: false,
+  allQuizzes: [],
+  selectedQuiz: {},
+};
+
+export const allQuizzesReducer = (state = allQuizzesInitialState, action) => {
+  switch (action.type) {
+    case "QUIZZES_LOADING":
+      return {
+        ...state,
+        loading: true,
+      };
+    case "QUIZZES_LOAD_SUCCEEDED":
+      return {
+        ...state,
+        allQuizzes: action.data,
+        loading: false,
+      };
+    case "QUIZZES_LOAD_FAILED":
+      return {
+        ...state,
+        allQuizzes: [],
+        loading: false,
+        failed: true,
+      };
+    case "SELECTED_QUIZ_ADDED":
+      return {
+        ...state,
+        selectedQuiz: state.allQuizzes.find((quiz) => quiz.id === action.data),
+      };
+    default: {
+      return state;
+    }
+  }
+};
+
+export const loadAllQuizzesAction = () => {
+  return async (dispatch) => {
+    dispatch({
+      type: "QUIZZES_LOADING",
+    });
+
+    try {
+      const response = await axios.get(`${apiUrl}/quizzes`);
+
+      dispatch({
+        type: "QUIZZES_LOAD_SUCCEEDED",
+        data: response.data,
+      });
+    } catch (error) {
+      dispatch({
+        type: "QUIZZES_LOAD_FAILED",
+      });
+      console.log(error.message);
+    }
+  };
+};
+
+export const selectedQuizAction = (value) => {
+  return {
+    type: "SELECTED_QUIZ_ADDED",
+    data: value,
+  };
+};
+
+export const getQuizzes = (state) => {
+  return state.quizzesList.allQuizzes;
+};
+
+export const getSelectedQuiz = (state) => {
+  return state.quizzesList.selectedQuiz;
+};
+
+export const getQuizzesLoading = (state) => {
+  return state.quizzesList.loading;
+};
+
+export const getQuizzesFailed = (state) => {
+  return state.quizzesList.failed;
+};
