da4c2cc199e738a5b88c18a644f84d19099065ed
julia <julia.albq@gmail.com>
2023-03-17 15:35:00 -0300

    add allQuizzes reducer to store

--- src/state/store.js
+++ src/state/store.js
@@ -1,10 +1,12 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { allQuizzesReducer } from "./allQuizzes";
 import { questionReducer } from "./question";
 import { quizReducer } from "./quiz";
 
 const reducer = combineReducers({
   quiz: quizReducer,
   question: questionReducer,
+  quizzesList: allQuizzesReducer,
 });
 
 export const store = configureStore({ reducer });
