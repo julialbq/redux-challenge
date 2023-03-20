16e8e350ef3edbf21c11f79b844b30293360f654
julia <julia.albq@gmail.com>
2023-03-20 14:49:34 -0300

    refactor: remove button to render quiz list

--- src/ui/components/QuizList.jsx
+++ src/ui/components/QuizList.jsx
@@ -1,4 +1,4 @@
-import "./QuizList.css"
+import "./QuizList.css";
 import { useDispatch, useSelector } from "react-redux";
 import {
   getQuizzes,
@@ -6,8 +6,8 @@ import {
   getQuizzesLoading,
   loadAllQuizzesAction,
 } from "../../state/allQuizzes";
-import { Button } from "./Button";
 import { Quiz } from "./Quiz";
+import { useEffect } from "react";
 
 export const QuizList = () => {
   const allQuizzes = useSelector((state) => getQuizzes(state));
@@ -15,19 +15,22 @@ export const QuizList = () => {
   const failed = useSelector((state) => getQuizzesFailed(state));
   const dispatch = useDispatch();
 
-  const loadQuizzes = () => {
-    dispatch(loadAllQuizzesAction());
-  };
+  useEffect(() => {
+    const loadQuizzes = () => {
+      dispatch(loadAllQuizzesAction());
+    };
+    loadQuizzes();
+  }, []);
+
   return (
     <div className="sidebar">
-      <Button type="button" onClick={loadQuizzes}>
-        Quizzes 
-      </Button>
       {loading && <p>Loading...</p>}
       {failed && <p>Try again</p>}
       <ul className="sidebar__list">
         {allQuizzes.length > 0 &&
-          allQuizzes.map(({ title, id }) => <Quiz key={id} title={title} id={id}/>)}
+          allQuizzes.map(({ title, id }) => (
+            <Quiz key={id} title={title} id={id} />
+          ))}
       </ul>
     </div>
   );
