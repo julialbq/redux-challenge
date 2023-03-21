69cfec529f204a5e40ad906ec926005a7b180fbd
julia <julia.albq@gmail.com>
2023-03-21 16:33:52 -0300

    refactor: remove unnecessary divs

--- src/ui/components/CreateQuestion.jsx
+++ src/ui/components/CreateQuestion.jsx
@@ -19,26 +19,25 @@ export const CreateQuestion = ({ onClick }) => {
   };
 
   return (
-    <div className="form-body__question">
-      <div className="form-body__question--title">
-        <Input
-          label="Question title"
-          type="text"
-          min={3}
-          placeholder="Title"
-          onChange={(e) => addQuestionTitle(e.target.value)}
-        />
-        <Select
-          name="questionType"
-          label="Select a question type"
-          onChange={(e) => changeQuestionType(e.target.value)}
-        >
-          <option value="">Select question type</option>
-          <option value="boolean">Yes or No</option>
-          <option value="string">Text</option>
-          <option value="number">Number</option>
-        </Select>
-      </div>
+    <>
+      <Input
+        label="Question title"
+        type="text"
+        min={3}
+        placeholder="Title"
+        onChange={(e) => addQuestionTitle(e.target.value)}
+      />
+      <Select
+        name="questionType"
+        label="Select a question type"
+        onChange={(e) => changeQuestionType(e.target.value)}
+      >
+        <option value="">Select question type</option>
+        <option value="boolean">Yes or No</option>
+        <option value="string">Text</option>
+        <option value="number">Number</option>
+      </Select>
+
       {
         <Button
           buttonType="secondary"
@@ -49,6 +48,6 @@ export const CreateQuestion = ({ onClick }) => {
           Add question
         </Button>
       }
-    </div>
+    </>
   );
 };
