3325058119b1ac251c7f307f951fd164d2edf958
julia <julia.albq@gmail.com>
2023-03-22 16:18:25 -0300

    feat: add Header component

---
+++ src/ui/components/Header.jsx
@@ -0,0 +1,10 @@
+import { StyledHeader } from "../style/Header"
+import { StyledTitleLarge } from "../style/typography"
+
+export const Header = () => {
+  return (
+    <StyledHeader>
+      <StyledTitleLarge>Noises and Quizzes</StyledTitleLarge>
+    </StyledHeader>
+  )
+}
