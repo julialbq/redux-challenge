790425cbec3c67b11c92414c513b2dde73289da2
julia <julia.albq@gmail.com>
2023-03-22 18:15:52 -0300

    style: change box style to be mobile first

--- src/ui/style/box.js
+++ src/ui/style/box.js
@@ -1,8 +1,7 @@
 import styled, { css } from "styled-components";
 
 export const StyledBox = styled.div`
-  width: ${(props) => (props.sidebar ? "30vw" : "60vw")};
-  padding: ${(props) => (props.sidebar ? "12px" : "20px")};
+  width: 300px;
   margin-top: ${(props) => (props.sidebar ? "10px" : "0")};
 
   display: flex;
@@ -14,15 +13,8 @@ export const StyledBox = styled.div`
   border: 1px solid var(--grey-10);
   border-radius: 8px;
 
-  ${(props) => {
-    switch (props.type) {
-      case "input-wrapper":
-        return css`
-         width: 100%;
-        `;
-
-      default:
-        return false;
-    }
-  }}
+  @media (min-width: 768px) {
+    width: ${(props) => (props.sidebar ? "30vw" : "60vw")};
+    padding: ${(props) => (props.sidebar ? "12px" : "20px")};
+  }
 `;
