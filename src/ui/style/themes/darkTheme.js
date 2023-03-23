162342a6c26d2ed592991fe77eab2fa5b8c63a28
julia <julia.albq@gmail.com>
2023-03-23 17:27:39 -0300

    feat: add objects with the application colors

---
+++ src/ui/style/themes/darkTheme.js
@@ -0,0 +1,12 @@
+export const darkTheme = {
+  primary: "#EDEDED",
+  secondary: "#1F1F1F",
+  primaryGrey: "#1F1F1F",
+  tertiary: "#35BB18",
+  signalRed: "#C82520",
+  signalRedButtonHover: "#EDEDED",
+  placeholder: "##A1A1A1",
+  buttonDisabledBckg: "##5E5E5E",
+  border: "#676767",
+  buttonDisabledBckgText: "#333333",
+};
